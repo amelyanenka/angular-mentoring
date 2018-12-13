68b2c1829e6db40e9f43a2b635ed6b00e9296241
Vadzim Yermalitski <vadzim_yermalitski@epam.com>
2018-12-14 01:18:44 +0300

    added markup for course component

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -12,24 +12,43 @@ export class CoursesService {
     return [
       {
         id: 1,
-        title: 'Course-1',
-        creation: 'Created-1',
-        duration: 'Duration-1',
-        description: 'Description-1'
+        title: 'IT Security Foundations: Core Concepts',
+        creation: '29.06.2018',
+        duration: '1h 45min',
+        description: 'Prepare for MTA Exam 98-367. Learn the core concepts of operating system and network-level security to avoid ' +
+        'threats and eliminate system vulnerabilities.'
       },
       {
         id: 2,
-        title: 'Course-2',
-        creation: 'Created-2',
-        duration: 'Duration-2',
-        description: 'Description-2'
+        title: 'Online Marketing Foundations',
+        creation: '15.07.2018',
+        duration: '1h 24min',
+        description: 'Learn today\'s online marketing techniques and find out how how to build a successful online marketing campaign' +
+        'for all digital channels: search, video, social, email, and display.'
       },
       {
         id: 3,
-        title: 'Course-3',
-        creation: 'Created-3',
-        duration: 'Duration-3',
-        description: 'Description-3'
+        title: 'Google Analytics Essential Training',
+        creation: '12.07.2018',
+        duration: '49min',
+        description: 'Learn how to use Google Analytics to find out how your website is performing. Discover how to use this powerful ' +
+        'platform to measure site traffic, ad performance, and social media activity.'
+      },
+      {
+        id: 4,
+        title: 'Programming Foundations: Real-World Examples',
+        creation: '10.07.2018',
+        duration: '1h 12min',
+        description: 'Learn how to connect programming theory to practice. This series of training videos explains basic programming ' +
+        'concepts such as functions, variables, and loops by comparing them to real-life scenarios.'
+      },
+      {
+        id: 5,
+        title: 'Learning Git and GitHub',
+        creation: '02.07.2018',
+        duration: '54min',
+        description: 'Learn to work with the most common Git commands, and use GitHub to clone, explore, and create templates from ' +
+        'existing projects.'
       }
     ];
   }
