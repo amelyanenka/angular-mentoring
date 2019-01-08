d3d02db7ea373043934cf260e7f1c8630b24eb8c
Vadzim Yermalitski <vadzim_yermalitski@epam.com>
2019-01-08 18:11:16 +0300

    added unit tests for course and courses components

--- src/app/courses/components/course/course.component.spec.ts
+++ src/app/courses/components/course/course.component.spec.ts
@@ -1,24 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CourseComponent } from './course.component';
+import { Course } from '../../entities/course';
+import { CourseInterface } from '../../interfaces/course.interface';
+import { CoursesComponent } from '../courses/courses.component';
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
   let fixture: ComponentFixture<CourseComponent>;
+  let coursesComponentSpy: jasmine.SpyObj<CoursesComponent>;
 
   beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('CoursesComponent', ['deleteItem']);
+
     TestBed.configureTestingModule({
-      declarations: [ CourseComponent ]
+      declarations: [ CourseComponent ],
+      providers: [ {provide: CoursesComponent, useValue: spy} ]
     })
     .compileComponents();
+
+    coursesComponentSpy = TestBed.get(CoursesComponent);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CourseComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('CourseComponent creation', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Open course method calling', () => {
+    const course: CourseInterface = new Course(1, 'stubTitle', 'stubCreation', 'stubDuration', 'stubDescription');
+    spyOn(component, 'openItem');
+    component.openItem(course);
+    expect(component.openItem).toHaveBeenCalled();
+  });
+
+  it('Delete course method calling', () => {
+    const course: CourseInterface = new Course(1, 'stubTitle', 'stubCreation', 'stubDuration', 'stubDescription');
+    spyOn(component, 'deleteItem');
+    component.deleteItem(course);
+    expect(coursesComponentSpy.deleteItem).toHaveBeenCalled();
   });
 });
--- src/app/courses/components/courses/courses.component.spec.ts
+++ src/app/courses/components/courses/courses.component.spec.ts
@@ -1,26 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoursesComponent } from './courses.component';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CoursesService } from '../../services/courses.service';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
+  let coursesServiceSpy: jasmine.SpyObj<CoursesService>;
 
   beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('CoursesService', ['getCourses']);
+
     TestBed.configureTestingModule({
       declarations: [ CoursesComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [ {provide: CoursesService, useValue: spy} ]
     })
     .compileComponents();
+
+    coursesServiceSpy = TestBed.get(CoursesService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CoursesComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('CoursesComponent creation', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Get all courses method calling', () => {
+    component.ngOnInit();
+    expect(coursesServiceSpy.getCourses).toHaveBeenCalled();
+  });
+
+  it('Show more method calling', () => {
+    spyOn(component, 'showMore');
+    component.showMore();
+    expect(component.showMore).toHaveBeenCalled();
   });
 });
