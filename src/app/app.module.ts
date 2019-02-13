016bd30c9c880435ef772794d70d44a9bc8d209b
Vadzim Yermalitski <vadzim_yermalitski@epam.com>
2019-02-13 18:32:24 +0300

    added simple routes for courses pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoursesModule } from './courses/courses.module';
-// import { AuthModule } from './auth/auth.module';
-// import { AddCourseModule } from './add-course/add-course.module';
+import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/components/courses/courses.component';
 
+const ROUTES: Routes = [
+  {path: '', redirectTo: 'courses', pathMatch: 'full'},
+
+  // redirect to
+  {path: 'courses', component: CoursesComponent},
+  {path: 'courses/:id', component: CoursesComponent},
+  {path: 'courses/new', component: CoursesComponent},
+
+  // 404
+  // {path: '**', component: NoContentComponent}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +24,7 @@ import { CoursesModule } from './courses/courses.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(ROUTES, {useHash: true}),
     SharedModule,
     CoursesModule,
     // AuthModule,
