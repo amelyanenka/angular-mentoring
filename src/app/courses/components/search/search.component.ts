be4746151654ccc5e6ddaf346009538cbfc8edda
Vadzim Yermalitski <vadzim_yermalitski@epam.com>
2019-03-02 22:45:54 +0300

    implemented new functionality for searching

--- src/app/courses/components/search/search.component.ts
+++ src/app/courses/components/search/search.component.ts
@@ -1,29 +1,41 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   public searchValue: string;
+  private searchSubject = new Subject<string>();
 
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
   @Output() changedSearchValue: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router) {}
 
-  onSearch(): void {
-    this.search.emit(this.searchValue);
+  public ngOnInit() {
+    this.searchSubject
+      .pipe(
+        filter(() => this.searchValue.length >= 3 || this.searchValue.length === 0),
+        debounceTime(1000)
+      )
+      .subscribe(() => this.search.emit(this.searchValue));
   }
 
-  onChangedSearchValue(searchValue: string): void {
+  public onSearch(): void {
+    this.searchSubject.next(this.searchValue);
+  }
+
+  public onChangedSearchValue(searchValue: string): void {
     this.searchValue = searchValue;
     this.changedSearchValue.emit(searchValue);
   }
 
-  onAddCourse(): void {
+  public onAddCourse(): void {
     this.router.navigate(['courses', 'new']);
   }
 }
